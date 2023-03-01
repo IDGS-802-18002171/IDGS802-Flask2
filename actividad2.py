4db3e99a547970b8cd796e9028d535ee22f791cd
Carlos Ruiz <18002171@alumnos.utleon.edu.mx>
2023-03-01 13:09:51 -0600

    decimo commit

--- actividad2.py
+++ actividad2.py
@@ -1,50 +1,84 @@
 from flask import Flask,render_template
 from flask import request
+from flask import make_response
+from flask import flash
 import forms2
 
 app=Flask(__name__)
+app.config['SECRET_KEY']="Esta es una clave encriptada"
 
 @app.route("/",methods=["GET","POST"])
 def Home():
     num_form=forms2.UserForm(request.form)
+    num_form1=forms2.UserForm1(request.form)
     ingles=''
     español=''
-    if request.method=='POST' :
+    if request.method=='POST' and num_form.validate():
         ingles=num_form.ingles.data
         ingles=ingles.upper()
         español=num_form.español.data
         español=español.upper()
-        f=open('diccionarioIngles.txt','a')
-        f.write('\n'+'{}'.format(ingles))
-        f.write('\n'+'{}'.format(español))
+        f=open('diccionario.txt','r')
+        contenido = f.read()
+        lista={}
+        lista2 = contenido.split('\n')
+        for item in lista2:
+            items=item.split(':')
+            lista['{}'.format(items[0])]=items[1]
+        if español in lista:
+            success_message='Error ya se encontraba guardada esa palabra o clave'
+            flash(success_message,category='error')
+        else:
+            f1=open('diccionario.txt','a')
+            try:
+                f1.write('\n'+'{}:{}'.format(español,ingles))
+                success_message='Agregado de forma exitosa'
+                flash(success_message,category='success')
+            except:
+                success_message='Error al agregar'
+                flash(success_message,category='error')
+            f1.close()
         f.close()
-    return render_template("home1.html",form=num_form)
+    return render_template("home1.html",form=num_form,form1=num_form1)
         
 
 @app.route("/resultado",methods=["POST"])
 def Resultado():
     num_form=forms2.UserForm(request.form)
-    idioma=request.form.get("rdb")
-    busqueda=num_form.busqueda.data
-    busqueda=busqueda.upper()
-    traduccion='No encontrada'
-    cont=0
-    f1=open('diccionario.txt','r')
-    contenido1 = f1.read()
-    lista1 = contenido1.split('\n')
-    f1.close()
-    if idioma=='E':
-        for item in lista1 :
-            if busqueda==item :
-                traduccion=lista1[(cont-1)]
-            cont=cont+1
-    cont=0
-    if idioma=='I':
-        for item in lista1 :
-            if busqueda==item :
-                traduccion=lista1[(cont+1)]
-            cont=cont+1
-    return render_template("home1.html",form=num_form,traduccion=traduccion)
+    num_form1=forms2.UserForm1(request.form)
+    if request.method=='POST' and num_form1.validate():
+        idioma=num_form1.radio.data
+        busqueda=num_form1.busqueda.data
+        busqueda=busqueda.upper()
+        f=open('diccionario.txt','r')
+        contenido = f.read()
+        lista={}
+        lista1={}
+        lista2 = contenido.split('\n')
+        for item in lista2:
+            items=item.split(':')
+            lista['{}'.format(items[0])]=items[1]
+        for item in lista2:
+            items=item.split(':')
+            lista1['{}'.format(items[1])]=items[0]
+        f.close()
+        if idioma=='E':
+            try:
+                traduccion=lista[busqueda]
+            except:
+                traduccion='No encontrada'
+        elif idioma=='I':
+            try:
+                traduccion=lista1[busqueda]
+            except:
+                traduccion='No encontrada'
+        if traduccion=='No encontrada':
+            success_message=traduccion
+            flash(success_message,category='error')
+        else :
+            success_message='El significado es: {}'.format(traduccion)
+            flash(success_message,category='success')
+    return render_template("home1.html",form=num_form,form1=num_form1)
 
 if __name__ == "__main__":
     app.run(debug=True,port=3000)
--- forms2.py
+++ forms2.py
@@ -7,5 +7,6 @@ from wtforms import validators
 class UserForm(Form):
     ingles=StringField('Ingles',[validators.DataRequired(message='Palabra requerida')])
     español=StringField('Español',[validators.DataRequired(message='Palabra requerida')])
+class UserForm1(Form):
     busqueda=StringField('Buscar...',[validators.DataRequired(message='Palabra requerida')])
-    
+    radio = RadioField('Idioma a traducir', choices=[('E', 'Ingles'), ('I', 'Español')])
